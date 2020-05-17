021b80751bd39280f169d999d4a6479f56ab773e
Takumi Sueda <puhitaku@gmail.com>
2020-05-17 13:31:25 +0900

    Describe metadata in setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-version = "0.0.1"
 
-setup(
-    name="r3build",
-    version=version,
-    author="Takumi Sueda",
-    author_email="puhitaku@gmail.com",
-    description="File change detection + automatic build",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    packages=find_packages(exclude=["test*"]),
-    test_suite="tests",
-)
+setup()
