81ec7f5f9c872342553c7368223752dd0aa8dfa8
Takumi Sueda <puhitaku@gmail.com>
2020-06-14 01:45:16 +0900

    Add flag to show available processors

--- r3build/__main__.py
+++ r3build/__main__.py
@@ -2,6 +2,7 @@
 import time
 import click
 from r3build.cli import R3build
+from r3build.processor import available_processors
 
 
 class ConfigOption(click.types.StringParamType):
@@ -17,7 +18,13 @@ class ConfigOption(click.types.StringParamType):
     type=ConfigOption(),
 )
 @click.option('-v', '--verbose', is_flag=True)
-def main(config, verbose):
+@click.option('--processors', is_flag=True)
+def main(config, verbose, processors):
+    if processors:
+        print('Available Processor ID (a.k.a. job type):')
+        print(''.join(f'* {i}\n' for i in available_processors.keys() if i != 'internaltest'))
+        return
+
     r3 = R3build(config_fn=config, verbose=verbose)
     r3.run()
 
