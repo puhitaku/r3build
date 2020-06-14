b9f29d1563a68b968f4f4551f0e1b11d39109a29
Takumi Sueda <puhitaku@gmail.com>
2020-06-14 15:43:41 +0900

    Add help text

--- r3build/__main__.py
+++ r3build/__main__.py
@@ -17,8 +17,8 @@ class ConfigOption(click.types.StringParamType):
     help='Configuration file. (default = r3build.toml)',
     type=ConfigOption(),
 )
-@click.option('-v', '--verbose', is_flag=True)
-@click.option('--list-types', is_flag=True)
+@click.option('-v', '--verbose', help='Verbose mode (equivalent to `log.all = true` in config)', is_flag=True)
+@click.option('--list-types', help='List available job types', is_flag=True)
 def main(config, verbose, list_types):
     if list_types:
         print('Available Job Types (a.k.a. processor IDs):')
