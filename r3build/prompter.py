77e4458795a7bbb92e8d7d204d6a6f0be177d9b2
Takumi Sueda <puhitaku@gmail.com>
2020-06-14 20:07:28 +0900

    Fix ellipsification

--- r3build/prompter.py
+++ r3build/prompter.py
@@ -5,17 +5,23 @@ class Prompter:
     def __init__(self, config):
         self.config = config
 
+    @staticmethod
+    def ellipsify(text, length=10):
+        if len(text) > length:
+            return text[:length-3] + "..."
+        return text + ' ' * (length - len(text))
+
     def accept(self, event):
-        cprint(f"R3BUILD Watcher >> Accept: {event}", "green")
+        cprint(f"R3BUILD Watcher    >> Accept: {event}", "cyan")
 
     def ignore(self, name, reason, event):
-        name = f'{name:7s}'[:7]
+        name = self.ellipsify(name)
         cprint(f"R3BUILD {name} >> Ignore: ({reason}) {event}", "yellow")
 
     def launch(self, name, event):
-        name = f'{name:7s}'[:7]
+        name = self.ellipsify(name)
         cprint(f"R3BUILD {name} >> Launch: {event}", "green")
 
     def result(self, name, info, color):
-        name = f'{name:7s}'[:7]
+        name = self.ellipsify(name)
         cprint(f"R3BUILD {name} >> {info}", color)
